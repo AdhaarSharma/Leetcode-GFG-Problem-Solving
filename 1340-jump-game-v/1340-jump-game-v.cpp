9c1ed8dda7398022f82c6ddd7278cbe02825a41a
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2022-01-08 07:17:08 +0530

    Time: 70 ms (24.08%), Space: 14.7 MB (83.23%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,18 +1,17 @@
 class Solution {
-
 public:
     int dp[1001] = {};
-    int dfs(vector<int>& arr, int i, int d, int res = 1) {
-        if (dp[i]) return dp[i];
-        for (auto j = i + 1; j <= min(i + d, (int)arr.size() - 1) && arr[j] < arr[i]; ++j)
-            res = max(res, 1 + dfs(arr, j, d));
-        for (auto j = i - 1; j >= max(0, i - d) && arr[j] < arr[i]; --j)
-            res = max(res, 1 + dfs(arr, j, d));
-        return dp[i] = res;
-    }
-    int maxJumps(vector<int>& arr, int d, int res = 1) {
-        for (auto i = 0; i < arr.size(); ++i)
-            res = max(res, dfs(arr, i, d));
+    int maxJumps(vector<int>& arr, int d, int res=1) {
+        for(auto i = 0; i < arr.size(); i++)
+            res = max(res, dfs(arr, d, i));
         return res;
+    }
+    int dfs(vector<int>& arr, int d, int i, int res = 1){
+        if(dp[i]) return dp[i];
+        for(auto j = i-1; j>=max(0, i-d) && arr[i]>arr[j]; j--)
+            res = max(res, 1+dfs(arr, d, j));
+        for(auto j = i+1; j<=min((int)arr.size()-1, i+d) && arr[i]>arr[j]; j++)
+            res = max(res, 1+dfs(arr, d, j));
+        return dp[i] = res;
     }
 };
