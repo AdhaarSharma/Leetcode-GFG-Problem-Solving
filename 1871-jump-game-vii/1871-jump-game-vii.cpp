992856933b774d61ae6060a57f703d6e12591c81
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2022-01-08 14:42:58 +0530

    Time: 56 ms (68.99%), Space: 15.8 MB (88.12%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool canReach(string s, int minJump, int maxJump) {
-        if (s.back() == '1') return false;
-        s[0] = '2'; // mark s[0] as reacheable
+        if(s.back() == '1') return false;
+        s[0] = '2';
         int j = 0;
-        for (int i = 0; i < s.size() && s.back() != '2'; ++i) {
-            if (s[i] != '2') continue; // only extend reacheable points
-            j = max(j, i + minJump); // `j` is at least `i + minJump`
-            while (j < s.size() && j - i <= maxJump) { // try to extend until `j > i + maxJump`
-                if (s[j] == '0') s[j] = '2'; // mark `s[j]` as reacheable if `s[j] == '0'`
-                ++j;
+        for(int i = 0; i<s.size() && s.back() != '2'; i++){
+            if(s[i] != '2') continue;
+            j = max(j, i + minJump);
+            while(j<s.size() && maxJump>=j-i){
+                if(s[j] == '0') s[j] = '2';
+                j++;
             }
         }
         return s.back() == '2';
