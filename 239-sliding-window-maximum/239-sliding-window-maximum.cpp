cb2b3b63c32f1ee58050c856c998f2dbc248c439
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2022-01-20 09:37:57 +0530

    Time: 458 ms (33.71%), Space: 145.7 MB (24.10%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
+    vector<int> maxSlidingWindow(vector<int> &nums, int k){
         vector<int> ans;
-        for (int i=0; i<nums.size(); i++) {
-            if (!dq.empty() && dq.front() == i-k) dq.pop_front();
-            while (!dq.empty() && nums[dq.back()] < nums[i])
-                dq.pop_back();
-            dq.push_back(i);
-            if (i>=k-1) ans.push_back(nums[dq.front()]);
+        priority_queue<pair<int,int>> heap;
+        for(int i=0; i<nums.size(); i++){
+            while(!heap.empty() && heap.top().second<=(i-k))
+                heap.pop();
+            heap.push(make_pair(nums[i],i));
+            if(i>=k-1)
+                ans.push_back(heap.top().first);
         }
         return ans;
     }
