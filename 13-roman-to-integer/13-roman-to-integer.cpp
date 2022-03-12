857f091644253ec7481815341083bc0e6bfc849a
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2022-03-13 00:57:51 +0530

    Time: 76 ms (5.17%), Space: 8.4 MB (15.90%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char,int> mp = {
+            {'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D',500}, {'M',1000} 
+        };
+        int a=0;
+        for(int i=0; i<s.length(); i++){
+            if(mp[s[i]]<mp[s[i+1]]){
+                a+=mp[s[i+1]]-mp[s[i]];
+                i++;
+            } else {
+                a+=mp[s[i]];
+            }
+            cout<<a<<endl;
+        }
+        return a;
+    }
+};
