3e0542c305596e880498231b3487f63e1de3cadb
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2022-03-13 01:00:57 +0530

    Time: 20 ms (44.57%), Space: 8.8 MB (9.38%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char,int> mp = {
+        unordered_map<char,int> mp = {
             {'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D',500}, {'M',1000} 
         };
         int a=0;
