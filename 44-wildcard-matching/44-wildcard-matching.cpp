db42a287ea9fd83fd5cd3f64f0fe4d0fc42956f9
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2022-06-01 16:21:32 +0530

    Time: 48 ms (75.47%), Space: 7.7 MB (82.34%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -4,14 +4,12 @@ public:
         
         bool dp[s.length()+1][p.length()+1];
         
-        dp[0][0] = true;
-        
         for( int i = 0 ;i <= s.length() ; i++ ){
             for( int j = 0 ; j <= p.length() ; j++ ){
                 //exception if the first character of p is * then 
                 if( i == 0 || j == 0 ){
                     if( i== 0 && j == 0 ){
-                        continue;
+                        dp[0][0] = true;
                     }else if( j == 0 ){
                         dp[i][j] = false;
                     }else if( p[j-1] == '*' ){
@@ -22,33 +20,14 @@ public:
                 }
                 
                 else if( p[j-1] == '*' ){
-                    
-                    if( dp[i-1][j] || dp[i][j-1] ){
-                        dp[i][j] = true;
-                    }else{
-                        dp[i][j] = false;
-                    }
+                        dp[i][j] = dp[i-1][j] || dp[i][j-1];
                 }else if( s[i-1] == p[j-1] || p[j-1] == '?'){
-                    if( dp[i-1][j-1] ){
-                        dp[i][j] = true;
-                    }else{
-                        dp[i][j] = false;
-                    }
+                        dp[i][j] = dp[i-1][j-1];
                 }else{
-                    
                     dp[i][j] = false;
                 }
             }
         }
-        
-//         for( int i = 0 ;i <= s.length() ; i++ ){
-//             for( int j = 0 ; j <= p.length() ; j++ ){
-//                 cout << dp[i][j] << " ";
-//             }
-//             cout << endl;
-//         }
-        
         return dp[s.length()][p.length()];
-        
     }
 };
