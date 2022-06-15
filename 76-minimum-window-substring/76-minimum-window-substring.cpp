ccb862b4e546c61ba5f1e3fdbbf642cd462cd97d
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2022-06-15 22:50:32 +0530

    Time: 7 ms (95.04%), Space: 8 MB (35.64%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.size() == 0 || t.size() == 0) return "";
-        vector<int> remaining(128, 0);
-        int required = t.size();
-        for (int i = 0; i < required; i++) remaining[t[i]]++;
-        
-        int min = INT_MAX, start = 0, left = 0, i = 0;
-        while(i <= s.size() && start < s.size()) {
-            if(required) {
-                if (i == s.size()) break;
-                remaining[s[i]]--;
-                if (remaining[s[i]] >= 0) required--;
-                i++;
-            } else {
-                if (i - start < min) {
-                    min = i -start;
-                    left = start;
-                }
-                remaining[s[start]]++;
-                if (remaining[s[start]] > 0) required++;
-                start++;
-            }
+        vector<int> cnt(256, 0);
+        int count = 0;
+        int start = 0;
+        int ans = INT_MAX;
+        for(auto ch:t){
+            if(cnt[ch] == 0)
+                count++;
+            cnt[ch]++;
         }
-        return min == INT_MAX? "" : s.substr(left, min);
+        int i=0, j=0;
+        while(j<s.length()){
+            cnt[s[j]]--;
+            if(cnt[s[j]] == 0)
+                count--;
+            if(count == 0){
+                while(count == 0){
+                    if(ans > j-i+1){
+                        ans = j-i+1;
+                        start = i;
+                    }
+                    cnt[s[i]]++;
+                    if(cnt[s[i]] > 0)
+                        count++;
+                    i++;
+                }
+            }
+            j++;
+        }
+        if(ans == INT_MAX)
+            return "";
+        else
+            return s.substr(start, ans);
     }
 };
