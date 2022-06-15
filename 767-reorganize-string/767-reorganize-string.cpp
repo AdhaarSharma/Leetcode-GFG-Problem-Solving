cbe64be3145781d65604eb663bfe68f81909d013
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2022-06-15 16:04:10 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (66.15%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int n = s.size();
+        if(n == 0)
+            return "";
+        vector<int> cnt(26, 0);
+        for(auto a : s)
+            cnt[a - 'a']++;
+        int max = 0;
+        char ch_max;
+        for(int i = 0; i<26; i++)
+            if(max < cnt[i]){
+                max = cnt[i];
+                ch_max = 'a' + i;
+            }
+        if(max > (n+1)/2)
+            return "";
+        int ind = 0;
+        string res(n,' ');
+        while(max){
+            res[ind] = ch_max;
+            ind+=2;
+            max--;
+        }
+        cnt[ch_max - 'a'] = 0;
+        for(int i=0; i<26; i++){
+            while(cnt[i]){
+                ind = ind>=n?1:ind;
+                res[ind] = i+'a';
+                ind+=2;
+                cnt[i]--;
+            }
+        }
+        return res;
+    }
+};
