c8e3b3cf78d1a1d1dcac52c79eb63fd701140392
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2022-06-30 01:22:04 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (28.47%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -4,17 +4,24 @@ public:
         if (dividend == INT_MIN && divisor == -1) {
             return INT_MAX;
         }
-        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
-        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
-        while (dvd >= dvs) {
-            long temp = dvs, m = 1;
-            while (temp << 1 <= dvd) {
-                temp <<= 1;
-                m <<= 1;
+        long long _dividend = dividend;
+        long long _divisor = divisor;
+        
+        int sign = ((_dividend < 0) ^
+                    (_divisor < 0)) ? -1 : 1;
+        cout << sign << endl;
+        _dividend = abs(_dividend);
+        _divisor = abs(_divisor);
+
+        long long quotient = 0, temp = 0;
+
+        for (int i = 31; i >= 0; --i) {
+
+            if (temp + (_divisor << i) <= _dividend) {
+                temp += _divisor << i;
+                quotient |= 1LL << i;
             }
-            dvd -= temp;
-            ans += m;
         }
-        return sign * ans;
+        return quotient*sign;
     }
 };
