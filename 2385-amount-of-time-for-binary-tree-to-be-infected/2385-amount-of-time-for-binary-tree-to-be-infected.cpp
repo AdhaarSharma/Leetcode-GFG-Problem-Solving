0aa83726e5e9182d18dc2bfcf1f9987c42363c64
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2022-08-30 01:04:40 +0530

    Time: 1186 ms (13.44%), Space: 185.3 MB (44.39%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -12,35 +12,43 @@
 class Solution {
 public:
 
-    int res = 0;
-    pair<bool,int> dfs(TreeNode* root, int start){
-        if(!root) return {false,0};
+    unordered_map<int,vector<int>> v;
 
-        pair<bool,int>p1 = dfs(root->left,start);
-        pair<bool,int>p2 = dfs(root->right,start);
+    void createGraph(TreeNode* root){
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,-1});
+        while(q.size()){
+            auto [node,parent]= q.front(); q.pop();
+            if(parent!=-1){
+                v[parent].push_back(node->val);
+                v[node->val].push_back(parent);
+            }
+            if(node->left)  q.push({node->left,node->val});
+            if(node->right) q.push({node->right,node->val});
+        }   
+    }
 
-        if(root->val==start){
-            int temp = max(p2.second,p1.second);
-            res = max(res,temp);
-            return {true,0};
+    int amountOfTime(TreeNode* root, int start) {
+        createGraph(root);
+
+        queue<int> q;
+        unordered_map<int,bool> seen;
+        q.push(start);
+        seen[start]=true;
+        int time=0;
+        for(;q.size();time++){
+            int n= q.size();
+            while(n--){
+                auto node= q.front();  q.pop();
+                for(auto i:v[node]){
+                    if(!seen[i]){
+                        q.push(i);
+                        seen[i]=true;
+                    }
+                }
+            }
         }
-
-        if(p1.first){
-            int sum = p1.second+p2.second+1;
-            res = max(res,sum);
-            return {true,p1.second+1};
-        }else if(p2.first){
-            int sum = p1.second+p2.second+1;
-            res = max(res,sum);
-            return {true,p2.second+1};
-        }
-
-        int sum = max(p1.second,p2.second);
-        return {false,1+sum};
+        return time-1;
     }
     
-    int amountOfTime(TreeNode* root, int start) {
-        dfs(root,start);
-        return res;
-    }
 };
