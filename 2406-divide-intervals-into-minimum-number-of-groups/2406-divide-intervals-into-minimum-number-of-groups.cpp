c12d494cdbf4f42a3bb6f8c51490cf1c98933e41
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2022-09-17 15:09:07 +0530

    Time: 405 ms (98.27%), Space: 105.2 MB (40.09%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int minGroups(vector<vector<int>>& in) {
-        
-        int n = in.size();
-        
-        int s[n],e[n];
-        
-        for(int i=0;i<n;i++) {
-            s[i] = in[i][0];
-            e[i] = in[i][1];
+    int minGroups(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        vector<int>start;
+        vector<int>end;
+        for(auto a:intervals){
+            start.push_back(a[0]);
+            end.push_back(a[1]);
         }
-        
-        sort(s,s+n);
-        sort(e,e+n);
-        
-        int req = 1,ans = 1,j = 0,i = 1;
-        
-        while(i<n and j<n){
-            if(s[i]<=e[j]){
-                req++;
-                i++;
-            }else{
-                req--;
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+        int ans = 1, result = 1;
+        int i=0, j=1;
+        while(i<n && j<n){
+            if(j<n && end[i]>=start[j]){
+                ans++;
                 j++;
             }
-            
-            ans = max(ans,req);
+            else{
+                ans--;
+                i++;
+            }
+            result = max(ans, result);
         }
+        return result;
         
-        return ans;
     }
 };
