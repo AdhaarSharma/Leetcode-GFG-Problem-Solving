860bb9fd79957a56e924ca63a899c8e07b6b79ad
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2023-02-20 00:08:55 +0530

    Time: 6 ms (38.34%), Space: 6.9 MB (15.09%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    vector<int> shortestToChar(string S, char C) {
-        int n = S.size(), pos = -n;
-        vector<int> res(n, n);
-        for (int i = 0; i < n; ++i) {
-            if (S[i] == C) pos = i;
-            res[i] = i - pos;
+    vector<int> shortestToChar(string s, char c) {
+        int n = s.length();
+        vector<int>left(n, INT_MAX), right(n, INT_MAX);
+        bool left1 = false, right1 = false;
+        for(int i=0; i<n; i++){
+            if(s[i] == c) left[i] = 0, left1 = true;
+            else if(left1 == true) left[i] = left[i-1] + 1;
+            if(s[n-i-1] == c) right[n-i-1] = 0, right1 = true;
+            else if(right1 == true) right[n-i-1] = right[n-i] + 1;
         }
-        for (int i = pos - 1; i >= 0; --i) {
-            if (S[i] == C)  pos = i;
-            res[i] = min(res[i], pos - i);
+        vector<int> ans(n,INT_MAX);
+        for(int i=0; i<n; i++){
+            ans[i] = min(left[i], right[i]);
         }
-        return res;
+        return ans;
     }
 };
