3309af017b08a80ff3f4a7802cdd7defc4a3bdc5
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2023-03-21 23:22:44 +0530

    Time: 158 ms (90.80%), Space: 107.5 MB (86.43%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    long long zeroFilledSubarray(vector<int>& nums) {
-        long long res = 0;
-        for (int i = 0, j = 0; i < nums.size(); ++i) {
-            if (nums[i] != 0)
-                j = i + 1;
-             res += i - j + 1;
+    long zeroFilledSubarray(vector<int>& nums) {
+        long cnt = 0, zeroSubarraysEndingAtCurrentIndex = 0;
+        for (int n : nums) {
+            if (n == 0) {
+                cnt += ++zeroSubarraysEndingAtCurrentIndex;
+            } else {
+                zeroSubarraysEndingAtCurrentIndex = 0;
+            }
         }
-        return res;
+        return cnt;
     }
 };
