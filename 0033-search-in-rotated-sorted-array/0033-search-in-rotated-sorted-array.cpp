bd207fe7173c0060992a416aac8a8f2bf58f97f4
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2023-04-01 23:31:25 +0530

    Time: 7 ms (34.79%), Space: 11.1 MB (82.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    int searchBS(vector<int>& nums, int target, int i, int j){
-        int l = i, h = j;
-        while(l<=h){
-            int mid = l + (h-l)/2;
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] > target){
-                h = mid - 1;
-            }
-            else {
-                l = mid + 1;
-            }
-        }
-        return -1;
-    }
     int search(vector<int>& nums, int target) {
         int n = nums.size(), l = 0, h = n-1;
         while(l <= h){
             int mid = l + (h-l)/2;
             if(nums[mid] == target)
                 return mid;
-            else if(nums[mid] > nums[h]){
+            else if(nums[mid] >= nums[h]){
                 if(nums[l] <= target && target < nums[mid])
-                    return searchBS(nums, target, l, mid - 1);
+                    h = mid - 1, cout<<h<<endl;
                 else
-                    l = mid + 1;
+                    l = mid + 1, cout<<l<<endl;
+            }
+            else if(nums[mid] <= nums[l]) {
+                if(nums[mid] < target && target <= nums[h])
+                    l = mid + 1, cout<<l<<endl;
+                else
+                    h = mid - 1, cout<<h<<endl;
             }
             else {
-                if(nums[mid] < target && target <= nums[h])
-                    return searchBS(nums, target, mid + 1, h);
-                else
+                if(nums[mid] > target){
                     h = mid - 1;
+                }
+                else {
+                    l = mid + 1;
+                }
             }
         }
         return -1;
