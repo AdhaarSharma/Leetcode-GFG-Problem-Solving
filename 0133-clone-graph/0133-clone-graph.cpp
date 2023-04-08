b2b4f0c5e127ccd01f2272d4363fb8805d6ef150
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2023-04-09 03:26:22 +0530

    Time: 3 ms (95.03%), Space: 8.8 MB (53.31%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* recur(Node* node, unordered_map<Node*,Node*>&visited){
+        Node* new_root = new Node(node->val);
+        visited[node] = new_root;
+        for(auto n:node->neighbors){
+            if(visited.find(n)!=visited.end())
+                new_root->neighbors.push_back(visited[n]);
+            else
+                new_root->neighbors.push_back(recur(n, visited));
+        }
+        return new_root;
+    }
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*,Node*> visited;
+        if(node==NULL)
+            return NULL;
+        if(node->neighbors.size()==0)
+        {
+            Node* clone= new Node(node->val);
+            return clone; 
+        }
+        return recur(node, visited);
+    }
+};
