7d321b49c3cc7afe3de2a86220019dc89ca788c8
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2023-09-11 11:46:31 +0530

    Time: 14 ms (35.21%), Space: 13.1 MB (56.43%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -5,10 +5,11 @@ public:
         vector<vector<int>> ans;
         vector<vector<int>> sizeMap(n+1,vector<int>());
         for(int i=0; i<n; i++){
-            sizeMap[groupSizes[i]].push_back(i);
-            if(sizeMap[groupSizes[i]].size()>=groupSizes[i]){
-                ans.push_back(sizeMap[groupSizes[i]]);
-                sizeMap[groupSizes[i]].clear();
+            vector<int> *group = &sizeMap[groupSizes[i]];
+            (*group).push_back(i);
+            if((*group).size()>=groupSizes[i]){
+                ans.push_back(*group);
+                (*group).clear();
             }
         }
         for(auto s:sizeMap){
