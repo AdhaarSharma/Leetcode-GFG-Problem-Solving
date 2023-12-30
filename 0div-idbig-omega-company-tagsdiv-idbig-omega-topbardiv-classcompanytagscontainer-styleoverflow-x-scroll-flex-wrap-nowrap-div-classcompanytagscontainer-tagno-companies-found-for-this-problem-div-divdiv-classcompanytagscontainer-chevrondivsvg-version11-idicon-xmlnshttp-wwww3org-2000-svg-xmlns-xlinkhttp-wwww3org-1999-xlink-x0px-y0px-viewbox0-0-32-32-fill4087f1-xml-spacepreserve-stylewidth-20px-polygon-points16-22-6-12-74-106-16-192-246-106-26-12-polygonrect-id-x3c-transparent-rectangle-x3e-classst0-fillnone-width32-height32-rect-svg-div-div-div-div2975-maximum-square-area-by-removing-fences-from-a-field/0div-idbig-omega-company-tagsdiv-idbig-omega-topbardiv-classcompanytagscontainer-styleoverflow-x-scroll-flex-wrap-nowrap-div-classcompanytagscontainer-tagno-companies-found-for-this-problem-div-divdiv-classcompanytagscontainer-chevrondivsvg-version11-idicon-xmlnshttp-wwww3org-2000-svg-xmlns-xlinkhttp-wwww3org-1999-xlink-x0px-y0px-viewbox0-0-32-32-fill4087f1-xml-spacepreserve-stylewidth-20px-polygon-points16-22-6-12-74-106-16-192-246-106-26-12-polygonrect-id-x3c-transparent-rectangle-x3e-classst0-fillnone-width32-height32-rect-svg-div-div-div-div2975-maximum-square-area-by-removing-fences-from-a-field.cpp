80dd978866de3d06d5dc5b42941db3a62a4eb450
AdhaarSharma <66046037+AdhaarSharma@users.noreply.github.com>
2023-12-31 02:16:47 +0530

    Time: 2006 ms (40.13%), Space: 210.9 MB (99.00%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2975-maximum-square-area-by-removing-fences-from-a-field/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2975-maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2975-maximum-square-area-by-removing-fences-from-a-field/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2975-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
-        // time = O(h^2 + v^2)
-        // space = O(max(h,v) + min(h,v)^2)
-        sort(hFences.begin(), hFences.end());
-        hFences.insert(hFences.begin(), 1);
+        hFences.push_back(1);
         hFences.push_back(m);
-        sort(vFences.begin(), vFences.end());
-        vFences.insert(vFences.begin(), 1);
+        vFences.push_back(1);
         vFences.push_back(n);
+        sort(hFences.begin(), hFences.end());
+        sort(vFences.begin(), vFences.end());
         if (hFences.size() < vFences.size())
-            // reduce space
             swap(hFences, vFences);
         unordered_set<int> vsize(vFences.size()*(vFences.size() - 1)/2);
         for (unsigned i = 0; i < vFences.size(); ++i)
             for (unsigned j = i + 1; j < vFences.size(); ++j)
                 vsize.insert(vFences[j] - vFences[i]);
         int maxs = -1;
-        //for (auto h: hsize) cout << h << ' '; cout << endl;
-        //for (auto h: vsize) cout << h << ' '; cout << endl;
         for (unsigned i = 0; i < hFences.size(); ++i)
             for (unsigned j = hFences.size(); --j > i; ) {
                 auto s = hFences[j] - hFences[i];
